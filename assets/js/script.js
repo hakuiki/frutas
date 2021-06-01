6e10dbec167aecb9f3a9711f1f5c1ea965c15117
Zachir <zachir13@gmail.com>
2021-05-31 21:31:04 -0400

    agregando cantidad de frutas en total

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,11 @@ var sumatoriaManzanas = raul + pedro_m
 
 var frutillasTotal = pedro_f / 2
 
-
-
+var frutasTotal = raul + pedro_m + pedro_f + alberto
 
 
 alert ("Hay esta cantidad de manzanas: " + sumatoriaManzanas);
 
 alert ("Hay esta cantidad de frutillas para dos personas: " + frutillasTotal)
 
+alert ("Hay esta cantidad de frutas en total: " + frutasTotal)
